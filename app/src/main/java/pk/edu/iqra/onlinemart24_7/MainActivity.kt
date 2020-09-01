8324864aeddd22ee0215d214b0e4714544870705
nabeel siddiqui <nabeelsiddiqui86@gmail.com>
2020-09-02 00:27:38 +0500

    Added Homepage Activity and some kotlin files for validation, data passing and small changes to signin/signup activity files and styles!

--- app/src/main/java/pk/edu/iqra/onlinemart24_7/MainActivity.kt
+++ app/src/main/java/pk/edu/iqra/onlinemart24_7/MainActivity.kt
@@ -16,5 +16,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+//to validate all the null fields in signin form and after signin Intent to homepage activity
+
+        btnsignin.setOnClickListener{
+            //        to get text
+            val eml = Utils.getText(ed_signin_Email)
+            val psw = Utils.getText(ed_signin_Password)
+
+            //  to validate null fields
+            if (Utils.validate(eml, psw)){
+                showToast("SignIn Successfully!")
+                val intent2 = Intent(this, HomepageActivity::class.java)
+                startActivity(intent2)
+            }
+            else {
+                showToast("Please fill all the empty fields!")
+            }
+        }
     }
 }
--- app/src/main/java/pk/edu/iqra/onlinemart24_7/SignupActivity.kt
+++ app/src/main/java/pk/edu/iqra/onlinemart24_7/SignupActivity.kt
@@ -16,5 +16,26 @@ class SignupActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+//to validate all the null fields in signup form and after signup Intent to signin activity
+        btn_signup.setOnClickListener{
+
+            //        to get text
+            val eml = Utils.getText(ed_signup_email)
+            val usr = Utils.getText(ed_signup_username)
+            val phn = Utils.getText(ed_signup_phone)
+            val cnc = Utils.getText(ed_signup_cnic)
+            val psw = Utils.getText(ed_signup_password)
+
+            //          to validate null fields
+            if (Utils.validate(eml, usr, phn, cnc, psw)){
+                showToast("SignUp Successfully!")
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                showToast("Please fill all the empty fields!")
+            }
+        }
     }
 }
