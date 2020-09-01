b015197af16526395a9475ebfdcc2583b3ea9696
nabeel siddiqui <nabeelsiddiqui86@gmail.com>
2020-09-02 04:02:17 +0500

    Added Firebase Authentication to project! and added some back-end logic in main activity and signup kotlin activity!.

--- app/src/main/java/pk/edu/iqra/onlinemart24_7/MainActivity.kt
+++ app/src/main/java/pk/edu/iqra/onlinemart24_7/MainActivity.kt
@@ -1,8 +1,11 @@
+//sign in activity main activity
 package pk.edu.iqra.onlinemart24_7
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signin.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,19 +23,53 @@ class MainActivity : AppCompatActivity() {
 //to validate all the null fields in signin form and after signin Intent to homepage activity
 
         btnsignin.setOnClickListener{
-            //        to get text
-            val eml = Utils.getText(ed_signin_Email)
-            val psw = Utils.getText(ed_signin_Password)
+//          calling SingInAuth
+            SignInAuth()
+        }
+    }
 
-            //  to validate null fields
-            if (Utils.validate(eml, psw)){
+
+//    fun for SignIn Authentication using firebase Authentication
+    private fun SignInAuth() {
+    //        to get text
+    val eml = Utils.getText(ed_signin_Email)
+    val psw = Utils.getText(ed_signin_Password)
+
+    Log.d("SignIn_Activity", "Email: $eml")
+    Log.d("SignIn_Activity", "Password: $psw")
+
+
+    //  to validate null fields
+    if (Utils.validate(eml, psw)) {
+//        showToast("SignIn Successfully!")
+//                val intent2 = Intent(this, HomepageActivity::class.java)
+//                startActivity(intent2)
+
+//        firebase authentication using email and password-sign In activity
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(eml, psw)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+//                else if successful
+                Log.d("SignIn", "Successfully SignIn with uid: ${it.result?.user?.uid}")
                 showToast("SignIn Successfully!")
-                val intent2 = Intent(this, HomepageActivity::class.java)
-                startActivity(intent2)
+
+//                after signup intent to sign in form
+                val to_homepage = Intent(this, HomepageActivity::class.java)
+                startActivity(to_homepage)
             }
-            else {
-                showToast("Please fill all the empty fields!")
+
+//                  message for  the unsuccessful SignIn
+//                  lambda expression
+            .addOnFailureListener {
+                Log.d("SignIn", "Failed To SignIn To Account, ${it.message}")
+                showToast("Failed To SignIn To Account, ${it.message}")
             }
+    }
+
+        else {
+            showToast("Please fill all the empty fields!")
         }
     }
 }
--- app/src/main/java/pk/edu/iqra/onlinemart24_7/SignupActivity.kt
+++ app/src/main/java/pk/edu/iqra/onlinemart24_7/SignupActivity.kt
@@ -2,7 +2,9 @@ package pk.edu.iqra.onlinemart24_7
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
@@ -19,23 +21,53 @@ class SignupActivity : AppCompatActivity() {
 
 //to validate all the null fields in signup form and after signup Intent to signin activity
         btn_signup.setOnClickListener{
+//          calling SignUpAuth fun
+            SignUpAuth()
+        }
+    }
 
-            //        to get text
-            val eml = Utils.getText(ed_signup_email)
-            val usr = Utils.getText(ed_signup_username)
-            val phn = Utils.getText(ed_signup_phone)
-            val cnc = Utils.getText(ed_signup_cnic)
-            val psw = Utils.getText(ed_signup_password)
 
-            //          to validate null fields
-            if (Utils.validate(eml, usr, phn, cnc, psw)){
-                showToast("SignUp Successfully!")
+
+//    fun for signup authentication using firebase
+    private fun SignUpAuth() {
+
+//            to get text
+    val eml = Utils.getText(ed_signup_email)
+    val usr = Utils.getText(ed_signup_username)
+    val phn = Utils.getText(ed_signup_phone)
+    val cnc = Utils.getText(ed_signup_cnic)
+    val psw = Utils.getText(ed_signup_password)
+
+//            to validate null fields
+    if (Utils.validate(eml, usr, phn, cnc, psw)){
+//                showToast("SignUp Successfully!")
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+
+//                firebase authentication using email and password-sign In activity
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(eml, psw)
+            .addOnCompleteListener{
+                //result, if isn't successful to create user account it return default activity with return message!
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+//                    else if successful
+                Log.d("SignUp", "Successfully created account with uid: ${it.result?.user?.uid}")
+                showToast("Account Created Successfully!")
+
+//                        after signup intent to sign in form
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
-            else {
-                showToast("Please fill all the empty fields!")
+//                  message for  the unsuccessful account creation
+//                  lambda expression
+            .addOnFailureListener{
+                Log.d("SignUp", "Failed To Create Account, ${it.message}")
+                showToast("Failed To Create Account, ${it.message}")
             }
-        }
     }
+    else {
+        showToast("Please fill all the empty fields!")
+    }
+}
+
 }
