644c69fb0929eabe24dae0dfe5e14248c8ed4273
Shahzaib Gauhar Khan 14944 <68869782+shahzaib-gauhar-khan-14944@users.noreply.github.co>
2020-09-07 06:25:16 +0500

    - Added Category Page - Added E-Wallet page - Added Checkout page

---
+++ app/src/main/java/pk/edu/iqra/onlinemart24_7/mywallet.kt
@@ -0,0 +1,11 @@
+package pk.edu.iqra.onlinemart24_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mywallet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mywallet)
+    }
+}
